2ceef8c73da4579fa72984419c4d4685636e5253
Denys Yepik <yepik.denis@gmail.com>
2023-04-25 21:24:19 +0300

    requirements.txt was filled

--- main.py
+++ main.py
@@ -16,16 +16,15 @@ class Guide_Builder:
         self.get()
         self.__get_guide_builder_data()
 
-
     def get(self):
         url = "https://www.youtube.com/feed/guide_builder"
         header = {
+            "accept-language": "en-US,en;q=0.9",
             "user-agent": ua.random
         }
         conn = requests.get(url, headers=header)
         self.html = conn.text
         conn.close()
-
 
     def __get_guide_builder_data(self):
         scripts = re.findall('<script[^>]*>(.*?)<\/script>', self.html)
@@ -35,22 +34,24 @@ class Guide_Builder:
             if re.search('webCommandMetadata.+guide_builder', script):
                 self.json_data = json.loads(re.findall('{.+}', script)[0])
 
+
 ua = UserAgent()
 
 
 if __name__ == "__main__":
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["videolang"]
+    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    # mydb = myclient["videolang"]
 
-    print(mydb.list_collection_names())
-
+    # print(mydb.list_collection_names())
 
     guide_builder = Guide_Builder()
     json_data = guide_builder.json_data
-    sectionListRenderer = json_data['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents']
+    sectionListRenderer = json_data['contents']['twoColumnBrowseResultsRenderer'][
+        'tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents']
     for itemSectionRenderer in sectionListRenderer:
-        print(itemSectionRenderer['itemSectionRenderer']['contents'][0]['shelfRenderer']['title']['runs'])
-        horizontalListRenderer = itemSectionRenderer['itemSectionRenderer']['contents'][0]['shelfRenderer']['content']['horizontalListRenderer']['items']
+        print(itemSectionRenderer['itemSectionRenderer']
+              ['contents'][0]['shelfRenderer']['title']['runs'])
+        horizontalListRenderer = itemSectionRenderer['itemSectionRenderer'][
+            'contents'][0]['shelfRenderer']['content']['horizontalListRenderer']['items']
         for gridChannelRenderer in horizontalListRenderer:
             print(gridChannelRenderer['gridChannelRenderer']['title'])
-
