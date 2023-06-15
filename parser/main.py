8a8f4c86188a64b40235b79ed966470b1a10e050
Денис Епик <yepik.denis@gmail.com>
2023-06-15 14:45:24 +0300

    get video list from channel (done)

--- parser/main.py
+++ parser/main.py
@@ -1,15 +1,97 @@
 import time
-# import pymongo
 import config
 
 import models
+import json
+import os
+import re
+
+from progress.bar import IncrementalBar
+
+
+def transcript(data):
+    transcription = json.loads(data)["actions"][0]["updateEngagementPanelAction"]["content"]["transcriptRenderer"]["content"]["transcriptSearchPanelRenderer"]["body"]["transcriptSegmentListRenderer"]["initialSegments"]
+    text = ''
+    for sentence in transcription:
+        try:
+            text = text + sentence["transcriptSegmentRenderer"]["snippet"]["runs"][0]["text"] + " "
+        except:
+            pass
+    return text
+
+
+def get_videos(channel):
+    channel.get_videos_pack()
+    while True:
+        print(len(channel.video_ids))
+        try:
+            channel.browse()
+        except BaseException:
+            break
+    print("\n")
+    print(channel.video_ids)
+    print(len(channel.video_ids))
 
 
 if __name__ == "__main__":
-    # myclient = pymongo.MongoClient(f"mongodb://{config.MONGO_INITDB_ROOT_USERNAME}:{config.MONGO_INITDB_ROOT_PASSWORD}@27017/")
-    # print(myclient.list_database_names())
+    # guide_builder = models.Guide_Builder()
+    # guide_builder.get()
 
-    guide_builder = models.Guide_Builder()
-    guide_builder.get()
+    # guide_builder.formated_data
+
+
+    # for category in guide_builder.formated_data:
+    #     try:
+    #         os.mkdir(f"{os.path.dirname(__file__)}/data/{category['title']}")
+    #     except FileExistsError:
+    #         pass
+    #     for channel_name in category["channels"]:
+            
+    #         # !!! NEED TO BE CHECKED FOR VALID FOLDER NAME !!!
+    #         channel = models.Channel(channel_name["username"])
+    #         channel.get()
+    #         get_videos(channel)
+
+    #         with open(f"{os.path.dirname(__file__)}/data/{category['title']}/{channel.channel_name}.json", "w") as file:
+    #             data = {
+    #                 "channel": channel.channel_name,
+    #                 "videos": {
+    #                         "videosCount": len(channel.video_ids),
+    #                         "list": channel.videos
+    #                     }
+    #             }
+    #             file.write(json.dumps(data))
+
+
+    channel = models.Channel("CHRISHERIA")
+    channel.get()
+    channel.get_all_videos()
+
+    with open(f"{os.path.dirname(__file__)}/data/{channel.channel_name}.json", "w") as file:
+        data = {
+            "channel": channel.channel_name,
+            "videos": {
+                    "videosCount": len(channel.video_ids),
+                    "list": channel.videos
+                }
+        }
+        file.write(json.dumps(data))
+
+    # for category in guide_builder.formated_data:
+    #     try:
+    #         print(category["title"] + "-------------------------")
+    #         for channel_data in category["channels"]:
+    #             username = channel_data["username"]
+    #             os.mkdir(username)
+    #             channel.get_videos(channel_data["link"])
+    #             for id in channel.video_ids:
+    #                 watch.get(id)
+    #                 watch.get_transcription()
+    #                 text = transcript(watch.transcription)
+    #                 with open(f"{username}/video_{id}.json", "w", encoding="utf8") as file:
+    #                     file.write(text)
+    #     except:
+    #         pass
     
-    print(guide_builder.formated_data)
+    
+    # print(channel.html)
--- parser/models.py
+++ parser/models.py
@@ -6,16 +6,176 @@ import json
 from fake_useragent import UserAgent
 
 
+ua = UserAgent()
+
+def search_within_scripts(regex, html):
+    # getting all script in html
+    scripts = re.findall('<script[^>]*>(.*?)<\/script>', html)
+    for script in scripts:
+        # searching for script that contain essential json data
+        if re.search(regex, script):
+            clear_json = re.findall('{.+}', script)[0]
+            return json.loads(clear_json)
+
+
+class Watch:
+    def __init__(self):
+        self.video_id = ''
+        self.header = {
+            "accept-language": "en-US,en;q=0.9",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19577"
+        }
+        self.params = dict()
+        self.json_data = None
+        self.transcription = None
+
+    def get(self, id):
+        self.video_id = id
+        print(self.header["user-agent"])
+        conn = requests.get(f"https://www.youtube.com/watch?v={id}", headers=self.header)
+        self.html = conn.text
+        conn.close()
+        self.json_data = search_within_scripts("getTranscriptEndpoint", self.html)
+        self.__parse_json()
+
+    def get_transcription(self):
+        with open("parser/get_transcription.json", "r") as file:
+            data = json.load(file)
+            data["params"] = self.json_data["engagementPanels"][-1]["engagementPanelSectionListRenderer"]["content"]["continuationItemRenderer"]["continuationEndpoint"]["getTranscriptEndpoint"]["params"]
+        conn = requests.post("https://www.youtube.com/youtubei/v1/get_transcript?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", headers=self.header, data=json.dumps(data))
+        self.transcription = conn.text
+        conn.close()
+
+    def __get_data(self):
+        # getting all script in html
+        scripts = re.findall('<script[^>]*>(.*?)<\/script>', self.html)
+        for script in scripts:
+            # searching for script that contain essential json data
+            if re.search('getTranscriptEndpoint', script):
+                clear_json = re.findall('{.+}', script)[0]
+                self.json_data = json.loads(clear_json)
+
+
+
+class Channel():
+    """
+        Creating channel model to get information from differant channels
+    """
+    def __init__(self, channel_name=None):
+        self.html = None
+        self.formated_data = dict()
+        self.header = {
+            "accept-language": "en-US,en;q=0.9",
+            "user-agent": ua.random
+        }
+        self.json_data = None
+        self.video_ids = []
+        self.videos = []
+        self.channel_name = channel_name
+        self._continuationItem = None
+        self._continuationToken = None
+
+    def get(self):
+        """
+            Enter channel name for example: mrbeast
+        """
+        
+        conn = requests.get(f"https://www.youtube.com/@{self.channel_name}", headers=self.header)
+        self.html = conn.text
+        conn.close()
+
+        self.json_data = search_within_scripts("videosCountText", self.html)
+        try:
+            self.__channel_parse_json()
+        except TypeError:
+            pass
+
+    def get_all_videos(self):
+        self.get_videos_pack()
+        while True:
+            print(len(self.video_ids))
+            try:
+                self.browse()
+            except BaseException:
+                break
+        print(len(self.video_ids))
+
+    def get_videos_pack(self):
+        conn = requests.get(f"https://www.youtube.com/@{self.channel_name}/videos", headers=self.header)
+        self.html = conn.text
+        conn.close()
+
+        self.json_data = search_within_scripts("browseEndpoint", self.html)
+        try:
+            self.__videos_pack_parse_json()
+        except:
+            pass
+
+    def browse(self):      
+        with open("parser/get_videos_content2.json", "r") as file:
+            data = json.load(file)
+            if self._continuationToken != None:
+                data["continuation"] = self._continuationToken
+
+        conn = requests.post("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", headers=self.header, data=json.dumps(data))
+        self.json_data = conn.json()
+        conn.close()
+
+        self.__browse_parse_json()
+
+    def __get_json_from_html_scripts(self, keyword, html):
+        # getting all script in html
+        scripts = re.findall('<script[^>]*>(.*?)<\/script>', html)
+        for script in scripts:
+            # searching for script that contain essential json data
+            if re.search(keyword, script):
+                clear_json = re.findall('{.+}', script)[0]
+                self.json_data = json.loads(clear_json)
+
+    def __channel_parse_json(self):
+        json_data = self.json_data
+        videosCountText = json_data["header"]["c4TabbedHeaderRenderer"]["videosCountText"]["runs"][0]["text"]
+        try:
+            self.videosCount = int(videosCountText)
+        except ValueError:
+            self.videosCount = 0
+
+    def __browse_parse_json(self):
+        json_data = self.json_data
+        contents = json_data["onResponseReceivedActions"][0]["appendContinuationItemsAction"]["continuationItems"]
+        self.__videos_parse_json(contents)
+
+
+    def __videos_pack_parse_json(self):
+        self.formated_data.clear()
+        json_data = self.json_data
+        category = json_data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]
+        if category["title"] == "Videos":
+            contents = category["content"]["richGridRenderer"]["contents"]
+            self.__videos_parse_json(contents)
+
+    def __videos_parse_json(self, contents):
+        for content in contents:
+            if "richItemRenderer" in content:
+                self.videos.append(content["richItemRenderer"])
+                self.video_ids.append(content["richItemRenderer"]["content"]["videoRenderer"]["videoId"])
+            elif "continuationItemRenderer" in content:
+                self._continuationItem = content
+                self._continuationToken = content["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"]
+                continuationTokenExist = True
+        if continuationTokenExist != True:
+            raise(BaseException())
+
+
 class Guide_Builder():
     def __init__(self):
         self.url = "https://www.youtube.com/feed/guide_builder"
         self.response = None
         self.json_data = None
-        self.formated_data = dict()
-        self.ua = UserAgent()
+        self.formated_data = []
         self.header = {
             "accept-language": "en-US,en;q=0.9",
-            "user-agent": self.ua.random
+            "user-agent": ua.random
         }
 
     def get(self):
@@ -24,7 +184,7 @@ class Guide_Builder():
         conn.close()
 
         # parse html and then update self.json_data
-        self.__get_guide_builder_data()
+        self.json_data = search_within_scripts("webCommandMetadata.+guide_builder", self.html)
         self.__parse_json()
 
     def __get_guide_builder_data(self):
@@ -46,6 +206,12 @@ class Guide_Builder():
             horizontalListRenderer = itemSectionRenderer['itemSectionRenderer']['contents'][0]['shelfRenderer']['content']['horizontalListRenderer']['items']
             for gridChannelRenderer in horizontalListRenderer:
                 channel_name = gridChannelRenderer['gridChannelRenderer']['title']['simpleText']
-                print(channel_name)
-                channel_list.append(channel_name)
-            self.formated_data[title] = channel_list
+                channel_link = gridChannelRenderer["gridChannelRenderer"]["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]
+                channel_list.append({"username": channel_name, "link": channel_link})
+            category = {
+                "title": title,
+                "channels": channel_list
+            }
+            self.formated_data.append(category)
+
+
